2b0335297448f2cd5b8c5043147f2c625bc6244f
Kyle Barlow <kb@kylebarlow.com>
2017-06-27 14:26:36 -0700

    All step GAM and png output

--- gam.R
+++ gam.R
@@ -15,12 +15,13 @@ print( paste0( 'Number of rows where total score sum does not match within toler
 output_dir = "output_R"
 dir.create(output_dir, showWarnings = FALSE)
 
-calc_gam <- function(df,by_labels) {
-    unique_name = paste(by_labels$PredictionRunName, by_labels$MutType, by_labels$ScoreMethodID, sep="-")
-    dir.create(file.path(output_dir, unique_name), showWarnings = FALSE)
+calc_gam <- function(df, by_labels, img_type) {
+    df_args <- c(by_labels, sep="-")
+    unique_name = do.call(paste, df_args)
     print( unique_name )
+    dir.create(file.path(output_dir, unique_name), showWarnings = FALSE)
 
-    zz = file( file.path( file.path(output_dir, unique_name), 'gam_summary.csv'), open = "wt")
+    zz = file( file.path( file.path(output_dir, unique_name), 'gam_summary'), open = "wt")
     sink(zz)
 
     gamobj <- gam( ExperimentalDDG ~ s(fa_atr, fx=TRUE, k=-1, bs="cs") + s(fa_elec, fx=TRUE, k=-1, bs="cs") + s(fa_rep, fx=TRUE, k=-1, bs="cs") + s(fa_sol, fx=TRUE, k=-1, bs="cs") + s(hbond_bb_sc, fx=TRUE, k=-1, bs="cs") + s(hbond_lr_bb, fx=TRUE, k=-1, bs="cs") + s(hbond_sc, fx=TRUE, k=-1, bs="cs"),
@@ -31,7 +32,16 @@ calc_gam <- function(df,by_labels) {
 
     gamsum = summary(gamobj)
     print(gamsum)
-    pdf( file.path( file.path(output_dir, unique_name), sprintf('gam_results-%s.pdf', unique_name)) )
+
+    if( img_type == 'pdf' ) {
+        pdf( file.path( file.path(output_dir, unique_name), sprintf('gam_results-%s.pdf', unique_name)) )
+    } else {
+        png(
+            file.path( file.path(output_dir, unique_name),
+                      sprintf('gam_results-%s.png', unique_name)),
+            width=20, height=20, units="cm", res=400
+        )
+    }
     par(mfrow=c(3,3))
     plot(
         gamobj,
@@ -42,13 +52,40 @@ calc_gam <- function(df,by_labels) {
            title=sprintf("GAM R: %.2f", sqrt(gamsum$r.sq) ),
            bty='n')
     dev.off()
+
+    if( img_type == 'pdf' ) {
+        pdf( file.path( file.path(output_dir, unique_name), sprintf('gam_results-residuals-%s.pdf', unique_name)) )
+    } else {
+        png(
+            file.path( file.path(output_dir, unique_name),
+                      sprintf('gam_results-residuals-%s.png', unique_name)),
+            width=20, height=20, units="cm", res=400
+        )
+    }
+    par(mfrow=c(3,3))
+    plot(
+        gamobj,
+        scheme=1,
+        residuals=TRUE
+    )
+    frame()
+    legend("topleft", legend=" ",
+           title=sprintf("GAM R: %.2f", sqrt(gamsum$r.sq) ),
+           bty='n')
+    dev.off()
+
     sink()
     close(zz)
     return( sqrt(gamsum$r.sq) )
 }
 
-corr_summary = all_data[,.( gamR=calc_gam(.SD, .BY), R=cor(total, ExperimentalDDG) ), by=.(PredictionRunName,ScoreMethodID,MutType)] # .SD is subset for each group by, .BY is group labels
+corr_summary = all_data[,.( gamR=calc_gam(.SD, .BY, 'pdf'), R=cor(total, ExperimentalDDG) ), by=.(PredictionRunName,ScoreMethodID,MutType)] # .SD is subset for each group by, .BY is group labels
 
-write.csv( corr_summary, file.path( output_dir, "corr_summary") )
+write.csv( corr_summary, file.path( output_dir, "corr_summary.csv") )
 
 print( corr_summary )
+
+print( "GAM on all backrub steps" )
+
+steps_corr_summary = all_data[,.( gamR=calc_gam(.SD, .BY, 'png'), R=cor(total, ExperimentalDDG) ), by=.(PredictionRunName,MutType)] # .SD is subset for each group by, .BY is group labels
+print( steps_corr_summary )
