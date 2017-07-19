799cecd4120e219064b91eca22cc0227bc2e387b
Kyle Barlow <kb@kylebarlow.com>
2017-07-19 16:44:15 -0700

    Saving averaged dfs

--- select_scores.py
+++ select_scores.py
@@ -3,6 +3,7 @@ import argparse
 import getpass
 import MySQLdb
 import os
+import sys
 import time
 
 from run_descriptions import all_runs
@@ -15,9 +16,26 @@ structure_orders = [
 ]
 
 dataframe_cache = '/dbscratch/kyleb/new_query_cache'
-categorized_query_cache = os.path.join( dataframe_cache, 'categorized_queries' )
-if not os.path.isdir( categorized_query_cache ):
-    os.makedirs( categorized_query_cache )
+desired_summed_and_averaged_columns = [
+    'PredictionRunName', 'DataSetID', 'PredictionID', 'ScoreMethodID', 'total', 'ExperimentalDDG', 'StructureOrder',
+]
+
+global_score_columns = [
+    'total',
+    'fa_atr', 'fa_dun', 'fa_elec', 'fa_intra_rep', 'fa_rep', 'fa_sol', 'hbond_bb_sc', 'hbond_lr_bb', 'hbond_sc', 'hbond_sr_bb', 'omega', 'p_aa_pp', 'pro_close', 'rama', 'ref', 'yhh_planarity', 'fa_dun_dev', 'fa_dun_rot', 'fa_dun_semi', 'fa_intra_atr_xover4', 'fa_intra_elec', 'fa_intra_rep_xover4', 'fa_intra_sol_xover4', 'hxl_tors', 'lk_ball', 'lk_ball_bridge', 'lk_ball_bridge_uncpl', 'lk_ball_iso', 'rama_prepro',
+    'cart_bonded',
+]
+
+id_columns = [
+    'PredictionID', 'DataSetID', 'PDBFileID', 'Subset', 'ExperimentalDDG',
+    'PredictionRunName',
+    'StructureOrder', # StructureOrder added by this script
+    # Last three IDs are special, as they are used for grouping
+    'ScoreMethodID', 'StructureID', 'ScoreType',
+]
+
+if not os.path.isdir( dataframe_cache ):
+    os.makedirs( dataframe_cache )
 
 def fetch_from_db_and_reorder( mysql_host, mysql_port, mysql_user, mysql_pass, mysql_db ):
     with open('scores_select.sql', 'r') as f:
@@ -95,9 +113,60 @@ def fetch_from_db_and_reorder( mysql_host, mysql_port, mysql_user, mysql_pass, m
                     new_query_df.rename( columns = {'NewStructureID' : 'StructureID'}, inplace = True )
                 new_query_df.to_csv( cached_query_df_path, compression = 'gzip' )
 
-            assert( 'MutType' not in new_query_df.columns )
-
     mysql_con.close()
+
+def sum_and_average():
+    df_paths = [ f for f in os.listdir(dataframe_cache) if '.csv' in f ]
+    paths_to_analyze = []
+    for benchmark_run in all_runs:
+        for structure_order in structure_orders:
+            matching_path = None
+            for df_path in df_paths:
+                if benchmark_run.prediction_set_name in df_path and structure_order + '.csv' in df_path:
+                    assert( matching_path == None )
+                    matching_path = df_path
+            assert( matching_path != None )
+            paths_to_analyze.append( (benchmark_run, structure_order, os.path.join( dataframe_cache, matching_path ) ) )
+
+    output_dir = os.path.join( dataframe_cache, 'summed_and_averaged' )
+    if not os.path.isdir( output_dir ):
+        os.makedirs( output_dir )
+
+    for benchmark_run, structure_order, df_path in paths_to_analyze:
+        print 'Summing and averaging:', df_path
+        df = pd.read_csv( df_path, compression = 'gzip' )
+
+        this_score_columns = [col for col in global_score_columns if col in df.columns]
+        score_and_id_columns = this_score_columns + id_columns
+        complex_scores_df = df[df.ScoreType.isin( ['MutantLPartner', 'MutantRPartner', 'WildTypeComplex'] )][score_and_id_columns]
+        for score_column in this_score_columns:
+            complex_scores_df.loc[:,score_column] *= -1
+        complex_scores_df = complex_scores_df.append(
+            df[df.ScoreType.isin( ['WildTypeLPartner', 'WildTypeRPartner', 'MutantComplex'] )][score_and_id_columns]
+        )
+        complex_scores_df = complex_scores_df.sort_index() #( columns = ['PredictionID', 'StructureID', ] )
+        summed_df_noindex = complex_scores_df.groupby(id_columns[:-1], as_index = False)[this_score_columns].sum()
+        summed_df = complex_scores_df.groupby(id_columns[:-1])[this_score_columns].sum().reset_index()
+
+        df_structure_orders = summed_df['StructureOrder'].drop_duplicates()
+        assert( len(df_structure_orders) == 1 )
+        structure_order = df_structure_orders[0]
+        max_struct_id = summed_df['StructureID'].max()
+
+        for num_structs in xrange( 1, max_struct_id + 1 ):
+            subset_summed_df = summed_df.loc[ summed_df['StructureID'] <= num_structs ]
+            avg_df = subset_summed_df.groupby(id_columns[:-2])[this_score_columns].mean().round(decimals=4).reset_index()
+            new_structure_order = structure_order + '_%02d' % num_structs
+            avg_df.assign( StructureOrder = new_structure_order )
+            avg_df = avg_df[desired_summed_and_averaged_columns].round(4)
+            avg_df.sort_values(
+                ['PredictionRunName', 'DataSetID', 'ScoreMethodID'],
+                inplace = True,
+            )
+
+            csv_path = os.path.join( output_dir, '%s-%s.csv.gz' % (benchmark_run.prediction_set_name, new_structure_order) )
+            avg_df.to_csv( csv_path, compression = 'gzip' )
+            print 'Saved:', csv_path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -114,3 +183,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     fetch_from_db_and_reorder( args.host, args.port, args.user, args.password, args.db)
+    sum_and_average()
