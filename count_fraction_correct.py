eab9f180ad6634b5f90e9908eb2dacadb00f7e8d
Kyle Barlow <kyle.barlow@adimab.com>
2018-01-22 17:28:12 -0800

    Version pasted and emailed to Tanja

--- count_fraction_correct.py
+++ count_fraction_correct.py
@@ -23,13 +23,13 @@ for csv_path, backrub_steps in input_data:
             assert( len(sub_df) == 1240 )
         print '%s subset (n=%d, backrub steps=%d)' % (mut_types[mut_type], len(sub_df), backrub_steps)
 
-        sub_df = sub_df.loc[ sub_df['ExperimentalDDG'] <= -1.0 ]
-        print 'Number of mutations with exp. ddG <= -1.0: %d' % len( sub_df )
+        sub_df = sub_df.loc[ sub_df['ExperimentalDDG'] > 0.0 ]
+        print 'Number of mutations with exp. ddG > 0: %d' % len( sub_df )
         print 'Number of the above predicted to be <= -1REU: %d' % len( sub_df.loc[ sub_df['total'] <= -1.0 ] )
         print 'Number of the above predicted to be < 0 REU: %d' % len( sub_df.loc[ sub_df['total'] < 0.0 ] )
-        print 'Number of the above predicted to be > 0 REU: %d' % len( sub_df.loc[ sub_df['total'] > 0.0 ] )
-        print 'Number of the above predicted to be 0 <= DDG < 1: %d' % len( sub_df.loc[ (sub_df['total'] >= 0.0) & (sub_df['total'] < 1.0) ] )
-        print 'Number of the above predicted to be >= 1 REU: %d' % len( sub_df.loc[ (sub_df['total'] >= 1.0) ] )
+        # print 'Number of the above predicted to be > 0 REU: %d' % len( sub_df.loc[ sub_df['total'] > 0.0 ] )
+        # print 'Number of the above predicted to be 0 <= DDG < 1: %d' % len( sub_df.loc[ (sub_df['total'] >= 0.0) & (sub_df['total'] < 1.0) ] )
+        # print 'Number of the above predicted to be >= 1 REU: %d' % len( sub_df.loc[ (sub_df['total'] >= 1.0) ] )
         print
 
         sub_df = df.loc[ df['MutType'] == mut_type ]
@@ -37,7 +37,7 @@ for csv_path, backrub_steps in input_data:
         print 'Number of mutations with exp. ddG >= 1.0: %d' % len( sub_df )
         print 'Number of the above predicted to be <= -1REU: %d' % len( sub_df.loc[ sub_df['total'] <= -1.0 ] )
         print 'Number of the above predicted to be < 0 REU: %d' % len( sub_df.loc[ sub_df['total'] < 0.0 ] )
-        print 'Number of the above predicted to be > 0 REU: %d' % len( sub_df.loc[ sub_df['total'] > 0.0 ] )
-        print 'Number of the above predicted to be 0 <= DDG < 1: %d' % len( sub_df.loc[ (sub_df['total'] >= 0.0) & (sub_df['total'] < 1.0) ] )
-        print 'Number of the above predicted to be >= 1 REU: %d' % len( sub_df.loc[ (sub_df['total'] >= 1.0) ] )
+        # print 'Number of the above predicted to be > 0 REU: %d' % len( sub_df.loc[ sub_df['total'] > 0.0 ] )
+        # print 'Number of the above predicted to be 0 <= DDG < 1: %d' % len( sub_df.loc[ (sub_df['total'] >= 0.0) & (sub_df['total'] < 1.0) ] )
+        # print 'Number of the above predicted to be >= 1 REU: %d' % len( sub_df.loc[ (sub_df['total'] >= 1.0) ] )
         print
